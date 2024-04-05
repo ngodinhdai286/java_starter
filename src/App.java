2e4a6fcc130c371a04ad9a77e3b3da69ebbd02ed
Dai Ngo <ngodinhdai77@gmail.com>
2024-04-05 15:43:14 +0700

    Các hàm xuất ra màn hình và nhập liệu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) {
+    // int age = 26;
+    // String name = "Ngo Dinh Dai";
+    // System.out.println("My age is: " + age + ", name is: " + name);
+    // System.out.printf("Có %d người đăng kí", 40000);
+    
+    // Hàm nhập
+    Scanner scanner  = new Scanner(System.in);
+    System.out.print("Your name is: ");
+    String name = scanner.nextLine();
+    System.out.print("Your age is: ");
+    int age = scanner.nextInt();
+    System.out.printf("My name is: %s\n", name);
+    System.out.printf("My age is: %d", age);
+    scanner.close();
+  }
 }
